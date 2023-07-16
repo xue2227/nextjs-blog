01dbdd2be18d864afcd3d6f35936ccade502493f
xue2227 <ya20020222@gmail.com>
2023-07-16 20:54:03 +0800

    Fix title bug

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
-        <title>{}siteTitle</title>
+        <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
